4ff8ad1046c1ad9145d1a847c7d7f65558afc80b
brunopenso <viperp@gmail.com>
2019-10-17 23:29:04 -0300

    code review changes

--- test/tags.test.js
+++ test/tags.test.js
@@ -16,14 +16,14 @@ test('getTags("my text here @")', function () {
 test('getTags("my text here @work")', function () {
   var r = tags.getTags('my text here @work')
   expect(r.length).toBe(1)
-  expect(r[0]).toBe('work')
+  expect(r[0]).toBe('@work')
 })
 
 test('getTags("my text here @work @prj1")', function () {
   var r = tags.getTags('my text here @work @prj1')
   expect(r.length).toBe(2)
-  expect(r[0]).toBe('work')
-  expect(r[1]).toBe('prj1')
+  expect(r[0]).toBe('@work')
+  expect(r[1]).toBe('@prj1')
 })
 
 test('removeTags("")', function () {
